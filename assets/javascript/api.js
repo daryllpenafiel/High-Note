8f50eceb53f50c57ca4f923ae49ab6d0ec3860e5
Wulfsbach <etemelkos@gmail.com>
2018-05-31 21:16:51 -0400

    working carousel

---
+++ assets/javascript/api.js
@@ -0,0 +1,45 @@
+function TicketMaster(){
+
+var queryURL= "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=527&apikey=pgJ6dICsI0gxvL7bAgiXrKr7p2I7pfZc"
+
+
+$.ajax({
+    
+    url: queryURL,
+    method: "GET",
+    async:true,
+    dataType: "json",
+    success: function(json) {
+                console.log(json);
+                // Parse the response.
+                var results = json._embedded.events;
+   
+                for( var i = 0; i < results.length; i++) {
+
+                var ConcertImage = $('<img src="'+ results[i].images[1].url +'">' );
+                $("#Hot").replaceWith(ConcertImage);
+                 
+
+              var Dates= $(results[i].dates.start);
+              
+                // var ConcertName= $(results[i].name);
+
+                // $("#Hot").prepend(ConcertName);
+                
+                
+                }
+
+                console.log(results);
+                // Do other things.
+             },
+    error: function(xhr, status, err) {
+                // This time, we do not end up here!
+             }
+  });
+
+
+
+
+}
+
+TicketMaster();
