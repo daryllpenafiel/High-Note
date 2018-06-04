78e0f51721f47b6be0171b857e9916c5675f537d
daryllpenafiel <daryll_penafiel@yahoo.com>
2018-06-03 22:31:25 -0400

    fixed search functionality

--- assets/javascript/searchboxjs.js
+++ assets/javascript/searchboxjs.js
@@ -1,30 +1,37 @@
 $(window).ready(function () {
 
     var artistSearch;
-    var retrievedKeyword = localStorage.getItem('keyword');
-
     getResults();
+
+    $("#spSearchButton").on("click", function () {
+        console.log("click");
+        document.location.href = './searchresults.html';
+        var keyword2 = $("#spSearch").val();
+        console.log(keyword2);
+        localStorage.setItem('keyword', keyword2);
+    });
 
     function getResults() {
 
-        if (retrievedKeyword) {
+        var retrievedKeyword = localStorage.getItem('keyword');
+
+        var searchInput = $("#search-page-search").val();
+        console.log(searchInput);
+
+        if (!retrievedKeyword) {
+            var searchInput = $("#search-page-search").val();
+            var artistSearch = searchInput;
+            console.log(artistSearch);
+        } else {
             artistSearch = retrievedKeyword;
             console.log(artistSearch);
             localStorage.clear();
-        } else if (!retrievedKeyword) {
-            artistSearch = $("#search-page-search").val();
-            console.log(artistSearch);
         }
 
-        //pass in parameter for getResults function
-        //oninputchjange => getresult (localstorege)
-
-        console.log(artistSearch);
         var APIkey = "STwEpnPDXERAZv6JNRKyv5EkAmxgukSN";
         var inputDate = new Date($("#dateinput").val()).getTime() / 1000;
 
         var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&apikey=" + APIkey + "&city=Toronto&sort=date,desc" + "&keyword=" + encodeURI(artistSearch);
-        console.log(queryURL);
 
         $.ajax({
             url: queryURL,
@@ -47,7 +54,6 @@ $(window).ready(function () {
                     var displayIMG = display[i].images[1].url;
                     var displayDate = display[i].dates.start.localDate;
                     var inputDate = $(".card-date").attr("data-date");
- 
 
                     var outputDiv = $("<div class='card' style='width: 20rem'>");
                     var outputIMG = $("<img class='card-img-top'>").attr("src", displayIMG);
@@ -61,52 +67,22 @@ $(window).ready(function () {
 
                     outputDiv.append(outputIMG);
                     outputDiv.append(outputDetails);
-                    //addWeather();
 
                     $("#output-page").prepend(outputDiv);
-  
+
                 }
             }
         })
-
-        $("#submitbutton").on("click", getResults);
-
-        $(document).on("click", ".dropdown-genre", function () {
-            event.preventDefault();
-            document.location.href = './searchresults.html';
-            localStorage.clear();
-            var keyword = $(this).attr("data-value");
-            console.log(keyword);
-            localStorage.setItem('keyword', keyword);
-        })
-
-        /*function addWeather() {
-            var APIkey = "db1f13174ea9e50874eb00cef8b7d022";
-            var stage = "43.629245900,-79.415239300";
-            var inputDate = $(".card-date").attr("data-date");
-            console.log(inputDate);
-
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function (response) {
-                var forecast = response.daily.data[0];
-                var date = forecast.time;
-                var forecast = response.daily.data[0];
-                var overview = forecast.summary;
-                var temperature = forecast.temperatureHigh;
-                var humidity = forecast.humidity;
-                var icon = forecast.icon;
-
-                return(temperature);
-                
-
-                var skycons = new Skycons({
-                    "color": "green"
-                });
-                skycons.add("icon1", icon);
-                skycons.play();
-            })
-        }*/
     }
+
+    $(document).on("click", ".dropdown-genre", function () {
+        //event.preventDefault();
+        document.location.href = './searchresults.html';
+        localStorage.clear();
+        var keyword = $(this).attr("data-value");
+        console.log(keyword);
+        localStorage.setItem('keyword', keyword);
+        console.log('dropdpown')
+    })
+
 })
